1b33445e512162810eab96b9b66d4db9859e9fdd
Dhruv jogani <skyllect.dhruv@gmail.com>
2023-07-12 14:06:27 +0530

    chekout validation

--- myapp/src/components/users/cart.js
+++ myapp/src/components/users/cart.js
@@ -3,6 +3,11 @@ import './user.css'
 import { useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { addtoCart, RemovetoCart } from "../../Services/Actions/actions"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
 let totalcost = 0
 let total = 0
@@ -17,6 +22,7 @@ function Cart() {
     const [area, setArea] = useState('');
     const [city, setCity] = useState('');
     const [pincode, setPincode] = useState('');
+    const [validated, setValidated] = useState(false);
 
     const dispatch = useDispatch()
     let prop = useSelector((a) => a.cardItems)
@@ -39,59 +45,68 @@ function Cart() {
 
 
 
-    const conformOrder = async () => {
-        const id = localStorage.getItem("id")
-        let formdata = new FormData()
+    const conformOrder = async (event) => {
+        const form = event.currentTarget;
+        console.log("Check :- ", form.checkValidity === false)
 
-        let swap
-        let counter = []
-        for (let i = 0; i < prop.length; i++) {
-            prop[i].cardData.quantity = 1
-            prop[i].cardData.fullName = fullName
-            prop[i].cardData.house = house
-            prop[i].cardData.area = area
-            prop[i].cardData.city = city
-            prop[i].cardData.pincode = pincode
-            for (var j = i + 1; j < prop.length; j++) {
-                if (prop[i].cardData._id === prop[j].cardData._id) {
-                    counter.push(prop[i])
-                    prop[i].cardData.quantity = prop[i].cardData.quantity + 1
-                    prop.splice(j, 1)
-                    j--
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        else {
+            const id = localStorage.getItem("id")
+            let formdata = new FormData()
+
+            let swap
+            let counter = []
+            for (let i = 0; i < prop.length; i++) {
+                prop[i].cardData.quantity = 1
+                prop[i].cardData.fullName = fullName
+                prop[i].cardData.house = house
+                prop[i].cardData.area = area
+                prop[i].cardData.city = city
+                prop[i].cardData.pincode = pincode
+                for (var j = i + 1; j < prop.length; j++) {
+                    if (prop[i].cardData._id === prop[j].cardData._id) {
+                        counter.push(prop[i])
+                        prop[i].cardData.quantity = prop[i].cardData.quantity + 1
+                        prop.splice(j, 1)
+                        j--
+                    }
                 }
             }
-        }
 
-
-        for (let k = 0; k < prop.length; k++) {
-            for (let l = k; l < prop.length; l++) {
-                if (prop[k].cardData._id >= prop[l].cardData._id) {
-                    swap = prop[l]
-                    prop[l] = prop[k]
-                    prop[k] = swap
+            for (let k = 0; k < prop.length; k++) {
+                for (let l = k; l < prop.length; l++) {
+                    if (prop[k].cardData._id >= prop[l].cardData._id) {
+                        swap = prop[l]
+                        prop[l] = prop[k]
+                        prop[k] = swap
+                    }
                 }
             }
+
+            formdata = [prop, email]
+
+            try {
+                await axios.post(`${process.env.REACT_APP_USER_URL}/checkout`, formdata,
+                    {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            token: token,
+                        },
+                    })
+            }
+            catch (e) {
+                console.log(e)
+            }
+            counter.map((e) => prop.push(e))
+            totalcost = 0
+            total = 0
+            window.location.href = '/user/shop'
         }
 
-
-        formdata = [prop, email]
-
-        try {
-            await axios.post(`${process.env.REACT_APP_USER_URL}/checkout`, formdata,
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        token: token,
-                    },
-                })
-        }
-        catch (e) {
-            console.log(e)
-        }
-        counter.map((e) => prop.push(e))
-        totalcost = 0
-        total = 0
-        window.location.href = '/user/shop'
+        setValidated(true);
     }
 
     let swap
@@ -119,61 +134,79 @@ function Cart() {
     }
 
     const hello = (product) => {
-        const { image, productName, price, quantity } = product
+        const { image, productName, price, quantity, stock } = product
         return (
-            <div className="i i_2">
-                <div className="img-wrapper item">
-                    <img src={process.env.REACT_APP_GET_IMAGE + image} alt="" />
-                </div>
-                <div className="text-wrapper item">
-                    <span>{productName}</span>
-                    <br />
-                    <span>Price ${price}</span>
-                </div>
-                <div className="button-wrapper item">
-                    <button value={JSON.stringify(product)} onClick={add}> + </button>
-                </div>
-                <div className="cart_quan">
-                    <p className="cart_quantity">{quantity}</p>
-                    <p className="cart_price">Total Price  {total = quantity * price}</p>
-                </div>
-                <div className="button-wrapper item">
-                    <button value={JSON.stringify(product)} onClick={remove}> - </button>
-                </div>
-                <div className="cartprop">{totalcost += quantity * price}</div>
+            <div id="showProductUser">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={process.env.REACT_APP_GET_IMAGE + image} id="imageSiza" />
+                    <Card.Body>
+                        <Card.Title>{productName}</Card.Title>
+                        <Card.Text>
+                            Price ::-- {price}
+                        </Card.Text>
+                        <Card.Text>
+                            InStock ::-- {stock}
+                        </Card.Text>
+                        <div id="cartButton">
+                            <Button variant="primary" value={JSON.stringify(product)} onClick={add}>+</Button>
+                            <p id="productCounter">{quantity}</p>
+                            <Button variant="primary" value={JSON.stringify(product)} onClick={remove}>-</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
             </div>
-        )
+        );
+    }
+
+    function GridComplexExample() {
+        return (
+            <Form noValidate validated={validated} onSubmit={conformOrder}>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Circled_times.svg/1200px-Circled_times.svg.png' onClick={check2} id='imageSizaClose' />
+                <Form.Group className="mb-3" controlId="formGridAddress1">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control placeholder="Full Name." name="productName" onChange={(e) => setfullName(e.target.value)} required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formGridAddress1" >
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control placeholder="12 - Floor, building name" onChange={(e) => setHouse(e.target.value)} required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formGridAddress2">
+                    <Row>
+                        <Col xs={7}>
+                            <Form.Control placeholder="Area" onChange={(e) => setArea(e.target.value)} required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a valid area.
+                            </Form.Control.Feedback>
+                        </Col>
+                        <Col>
+                            <Form.Control placeholder="City" onChange={(e) => setCity(e.target.value)} required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a valid city.
+                            </Form.Control.Feedback>
+                        </Col>
+                        <Col>
+                            <Form.Control type="number" min={111111} max={999999} placeholder="Pincode" onChange={(e) => setPincode(e.target.value)} required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a valid pincode  .
+                            </Form.Control.Feedback>
+                        </Col>
+                    </Row>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
+        );
     }
 
 
     return (
-        <div className="items position">
+        <div className="items">
             {showTag ?
-                <div id='orderconfrom'>
-                    <label id='updateclose'>
-                        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Circled_times.svg/1200px-Circled_times.svg.png' onClick={check2} />
-                    </label>
-                    <label>
-                        Full Name <input type="text" placeholder="Full name" name="productName" onChange={(e) => setfullName(e.target.value)} />
-                    </label>
-                    <label>
-                        House <input type="text" placeholder="Building No and Name" name="productName" onChange={(e) => setHouse(e.target.value)} />
-                    </label>
-                    <label>
-                        Area <input type="text" placeholder="Area" name="price" onChange={(e) => setArea(e.target.value)} />
-                    </label>
-                    <label>
-                        City <input type="text" placeholder="City" name="price" onChange={(e) => setCity(e.target.value)} />
-                    </label>
-                    <label>
-                        Pincode <input type="tel" max={6} min={6} placeholder="Pincode" name="price" onChange={(e) => setPincode(e.target.value)} />
-                    </label>
-                    <label id='submit'>
-                        <input type='submit' onClick={conformOrder} />
-                    </label>
-                </div> :
-                <div id='orderconfrom1'>{prop.map((product) => hello(product.cardData))}
-                    <div className="totalcost">
+                <div id='orderconfrom'> {GridComplexExample()} </div>
+                : <div>
+                    <div id="totalcost">
                         {
                             prop.length != 0 ?
                                 <>
@@ -183,7 +216,12 @@ function Cart() {
                                 : <h1>Add product</h1>
                         }
                     </div>
-                    <div className="cartprop">{counter.map((e) => prop.push(e))} {totalcost = 0} {total = 0}</div></div>}
+                    <div className='position'>
+                        {prop.map((product) => hello(product.cardData))}
+                    </div>
+                    <div className="cartprop">{counter.map((e) => prop.push(e))} {totalcost = 0} {total = 0}</div>
+                </div>
+            }
         </div>
     )
 }
