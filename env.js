8332be2c6bfccbee241d76d9605861f7cc0d04c5
Dhruv jogani <skyllect.dhruv@gmail.com>
2023-07-14 16:48:03 +0530

    env and .file

---
+++ env.js
@@ -0,0 +1,20 @@
+// client
+
+nodemodule
+
+REACT_APP_ADMIN_URL = "http://localhost:4200/admin"
+REACT_APP_CEO_URL = "http://localhost:4200/ceo"
+REACT_APP_USER_URL = "http://localhost:4200/user"
+REACT_APP_LOGIN_URL = "http://localhost:4200/login"
+REACT_APP_REGISTER_URL = "http://localhost:4200/register"
+REACT_APP_GET_IMAGE = "http://localhost:4200/image?path="
+
+
+// server side
+
+nodemodule
+
+SERVER_PORT = 4200
+JWT_SECRET_KEY= 'secretKey'
+
+DB_HOST="mongodb+srv://dhruvjogani123:dhruvjogani123@cluster0.xoxcqh4.mongodb.net/ecom"
