6d3bda8f321555da4410aadcd7268bf4bcf1fa4d
Akshay Patel <patelaxay177@gmail.com>
2023-09-13 11:27:05 +0530

    update

--- one1.js
+++ one1.js
@@ -1,3 +1,19 @@
-// learn and practice procedure and trigger
-// connect mysql with nodejs
-// start learn function in mysql
+
+const [first, setFirst] = useState(false);
+const [second, setSecond] = useState(false);
+
+const restBtn = () => {
+    if (true) {
+        setFirst(''); 
+        setSecond('');
+    }
+}
+
+<div className="calc">
+    FIRST<input type="number" placeholder="first" onChange={(e) => { setFirst(e.target.value) }} />
+    SECOND<input type="number" placeholder="second" onChange={(e) => { setSecond(e.target.value) }} />
+    F * S<input value={first * second} />
+    F * S / 9<input value={(first * second / 9).toFixed(2)} />
+    F - S<input value={(first - second)} />
+    F / 2<input value={(first / 2)} />
+</div>
